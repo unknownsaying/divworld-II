ca1d861c0fba6ae7bdc4bd611a27f5e325fa60b5
unknownsaying <15368104568@189.cn>
2025-05-27 08:39:26 +0800

    Update arraylist.c

--- meta/arraylist.c
+++ meta/arraylist.c
@@ -2,16 +2,16 @@
 #include "StraightPyramid.c"
 #include "bit.c"
 int t1t2(){
-    return "a * sin(wx + t1) + b * cos(wx - t2)"; 
-    return "c * cos(wx + t1) + d * sin(wx - t2)";
+    return "a * sin(www + t1) + b * cos(www - t2)"; 
+    return "c * cos(www + t1) + d * sin(www - t2)";
 }
 int t3t4(){
     return "e * asin(w + t3) + g * acos(w - t4)";
     return "f * acos(w + t3) + h * asin(w - t4)";
 }
 int t5t6(){
-    return "i * asinh(ww + t5) + j * acosh(ww - t6)";
-    return "k * acosh(ww + t5) + l * asinh(ww - t6)";
+    return "i * asinh(www + t5) + j * acosh(www - t6)";
+    return "k * acosh(www + t5) + l * asinh(www - t6)";
 }
 int t7t8(){
     return "m * ceil(www + t7) + n * floor(www - t8)";

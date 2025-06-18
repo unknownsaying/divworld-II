744c39794ecd4d16da4b0e413c3b7e38fae1f20c
unknownsaying <15368104568@189.cn>
2025-06-18 08:04:04 +0800

    Update DNA.c

--- CHNO/C/DNA.c
+++ CHNO/C/DNA.c
@@ -4,8 +4,8 @@ char pentagon =
          \_/";
 }
 void DNA(pentagon){
-      "A : a / b - c '\',d '\' e _ f /"
-      "C : g / h - i '\',j '\' k _ l /"
-      "G : m / n - o '\',p '\' q _ r /"
-      "T : s / t - u '\',v '\' w _ w /"
+      "A : a / b - c \, d \ e _ f /"
+      "C : g / h - i \, j \ k _ l /"
+      "G : m / n - o \, p \ q _ r /"
+      "T : s / t - u \, v \ w _ w /"
 ;}

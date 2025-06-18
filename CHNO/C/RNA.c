2199d346ccdd2ab32c64a8c91d8b2d1f0b4a3e6d
unknownsaying <15368104568@189.cn>
2025-06-18 08:04:23 +0800

    Update RNA.c

--- CHNO/C/RNA.c
+++ CHNO/C/RNA.c
@@ -4,8 +4,8 @@ char hexagon =
      \_/";
 }
 void RNA(hexagon){
-   "A : a / b '\' c '\' d _ e /"
-   "C : f / g '\' i '\' j _ k /"
-   "G : l / m '\' n '\' o _ p /"
+   "A : a / b \ c \ d _ e /"
+   "C : f / g \ i \ j _ k /"
+   "G : l / m \ n \ o _ p /"
    "U : x | y | z"
 ;}
